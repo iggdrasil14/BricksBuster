49826c15c7e1ae52f1d02382f745bb5bea864c08
iggdrasil14 <iggdrasil14@gmail.com>
2024-11-28 21:15:23 +0700

    Bricks Buster - day 1
    Доработан скрипт MovingBallб добавлены условия падения шара в мертвую зону.

---
+++ Assets/Scripts/BrickTemplate.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickTemplate : MonoBehaviour
+{
+    public GameObject Prefab;
+    public List<Sprite> Sprite;
+    public Color color;
+    public int score;
+    public int healthBrick;
+    private int _countBlocks = 0;
+
+    void Start()
+    {
+        healthBrick = 1;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        _countBlocks++;
+    }
+
+    private void OnDisable()
+    {
+        _countBlocks--;
+        if(_countBlocks <= 0)
+        {
+            Debug.Log("Блоки закончились");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
--- Assets/Scripts/MovingBall.cs
+++ Assets/Scripts/MovingBall.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class MovingBall : MonoBehaviour
 {
     public Rigidbody2D rb;                                                                      // Переменная, которая ссылается на компонент Rigidbody 2D.
+    public GameObject platform;
+    public GameObject ball;
+    public GameObject platformAndBallPrefab;
     public bool isActive;                                                                       // Состояние мяча (деактивирован в начале игры, активирован после нажатия ЛКМ).
+    private bool _isFall = false;
     public float Force = 500f;                                                                  // Сила с которой мяч начинает двигаться.
     public float OffsetX = 100f;                                                                // Смещение мяча по оси X.
     public float speedBall;
+    public int _playersLife;                                                                    // Количество жизней игрока
     private Vector2 _inDirection;
     private Vector2 _inNormal;
+    private Vector2 _startPosition;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();                                                       // Сохранение в переменной rb ссылку на компонент Rigidbody.
         rb.bodyType = RigidbodyType2D.Kinematic;                                                // Установка режима Kinematic чтобы в начале игры мяч мог двигаться с платформой.
+        _playersLife = 3;
     }
 
     void Update()
@@ -40,8 +47,54 @@ public class MovingBall : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Движение шара.
         speedBall = _inDirection.magnitude;                                                     // Сохранение текущей скорости мяча (величину вектора скорости).
         var direction = Vector2.Reflect(_inDirection.normalized, collision.contacts[0].normal); // Расчет нового направления движения мяча.
         rb.velocity = direction * speedBall;                                                    // Установка новой скорости мяча после отскока.
+        
+        // Уничтожение объекта.
+        if (collision.gameObject.CompareTag("Brick"))                                           // Проверка, содержит ли объект тег Brick.
+        {
+            Destroy(collision.gameObject);                                                      // Уничтожение объекта содержащего тег Brick.
+        }
+
+        // Уничтожение шара, потеря жизни игрока, окончание игры.
+        if (collision.gameObject.CompareTag("DeathZone"))                                       // Проверка, содержит ли объект тег DeathZone.
+        {
+            Destroy(ball);                                                                      // Уничтожает объект ball.
+            Destroy(platform);                                                                  // Уничтожает объект platform (старая).
+            Instantiate(platformAndBallPrefab, new Vector2(0, -8), Quaternion.identity);        // Инициализация префаба FlatformAndBall в начальный координатах.
+            _isFall = true;
+        }
+    }
+
+    /// <summary>
+    /// Метод увеличения/уменьшения количества жизней у игрока.
+    /// </summary>
+    public void PlayersLife()
+    {
+        // Условия увеличения количества жизней.
+        //if ()
+        //{
+        //    _playersLife++;
+        //}
+
+        // Условие уменьшения количества жизней.
+        if(_isFall == true)
+        {
+            _playersLife--;
+            GameOver();
+        }
+    }
+
+    /// <summary>
+    /// Метод проверки окончания игры.
+    /// </summary>
+    public void GameOver()
+    {
+        if(_playersLife <= 0)
+        {
+            Debug.Log("Game Over");
+        }
     }
 }
