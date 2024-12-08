e1770c962e4642b9078fe1d97468b5a7947c74a9
iggdrasil14 <iggdrasil14@gmail.com>
2024-12-08 13:39:39 +0700

    Bricks Buster - day 4
    Настройка кнопки Exit, создал сцену Hall of Fame, настроил кноку Hall of Fame, настроил кнопку (back) возврата в MainMenu из сцены.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -23,14 +23,28 @@ public class SceneLoader : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Загрузить сцену с Залом Славы.
+    /// </summary>
     public void HallOfFame()
     {
-
+        SceneManager.LoadScene(2);
     }
 
+    /// <summary>
+    /// Кнопка Back из сцены Hall of Fame которая активирует возврат в MainMenu.
+    /// </summary>
+    public void HallOfFameBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    /// <summary>
+    /// Выйти из игры.
+    /// </summary>
     public void Exit()
     {
-
+        Application.Quit();
     }
 
 }
